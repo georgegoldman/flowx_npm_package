ae8927343907b9ab576630b333287433b365b175
georgegoldman <georgegoldmanjohn.o@gmail.com>
2024-11-29 03:36:01 +0100

    added the neccesary things

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
-import path from 'path'; // Use ES module import for path
+import path from 'path'; // ES module import
 
 export default {
-  // other Webpack configurations...
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'], // adjust as necessary
     fallback: {
-      path: require.resolve('path-browserify'), // Polyfill for 'path' module
-      fs: false, // Exclude 'fs' module for the browser
-      os: require.resolve('os-browserify/browser'), // Exclude 'os' module for the browser
-      crypto: require.resolve("crypto-browserify"),
+      path: require.resolve('path-browserify'), // Use require.resolve for static resolution
+      fs: false, // Exclude fs module for the browser
+      os: require.resolve('os-browserify/browser'), // Use os-browserify for os module
+      crypto: require.resolve('crypto-browserify'), // Use crypto-browserify for crypto module
       util: require.resolve('util/'), // Polyfill for 'util' module if needed
     },
   },
-  // other configurations...
 };
